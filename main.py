25689fa62e423997ea5db92e8452b17715a7a0cc
Thiago Barreto <thiagobarreto0705@gmail.com>
2025-04-25 21:56:03 -0300

    style: renomeando constantes para maiúsculas

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ archive = 'data.xlsx'
 
 df = pd.read_excel(archive, sheet_name=0)
 
-user = ''
-password = ''
-host = ''
-port = ''
-db = ''
-table = ''
+USER = ''
+PASSWORD = ''
+HOST = ''
+PORT = ''
+DB = ''
+TABLE = ''
 
-engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}')
+engine = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}')
 
-df.to_sql(table, engine, if_exists='append', index=False)
+df.to_sql(TABLE, engine, if_exists='append', index=False)
 
 print("Dados importados com sucesso!")
